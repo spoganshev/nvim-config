62e8272bd19cc79a5d7a804053ee1f6d146c0326
Sergei Poganshev <sergei.poganshev.consultant@nielseniq.com>
2025-09-04 13:28:15 +0300

    feat(lazy-plugins.lua): add snacks plugin for input

---
+++ lua/kickstart/plugins/snacks.lua
@@ -0,0 +1,10 @@
+-- A collection of small QoL plugins for Neovim.
+-- https://github.com/folke/snacks.nvim?tab=readme-ov-file
+
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    input = {},
+  },
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -58,6 +58,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.fugitive',
+  require 'kickstart.plugins.snacks',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
