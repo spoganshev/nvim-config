fa052641537e3d35008d4949d92fc5d7a8ce227f
Sergei Poganshev <sergei.poganshev.consultant@nielseniq.com>
2025-09-08 00:38:16 +0300

    feat(fugitive.lua): add fugitive github and gitlab plugins

--- lua/kickstart/plugins/fugitive.lua
+++ lua/kickstart/plugins/fugitive.lua
@@ -1,7 +1,21 @@
--- See https://github.com/tpope/vim-fugitive (Git wrapper for NVIM)
 return {
   {
+    -- See https://github.com/tpope/vim-fugitive (Git wrapper for NVIM)
     'tpope/vim-fugitive',
+    keys = {
+      { '<leader>hB', ':GBrowse<CR>', desc = 'git [B]rowse', silent = true },
+    },
+  },
+  {
+    -- Fugitive plugin for GitHub
+    'tpope/vim-rhubarb',
+  },
+  {
+    -- Fugitive plugin for GitLab
+    'shumphrey/fugitive-gitlab.vim',
+    config = function()
+      vim.g.fugitive_gitlab_domains = { ['scm.slicetest.com'] = 'https://scm.slicetest.com' }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
