10cfa6e19d3acab8cd5772e7613c8a4c54bbe515
Sergei Poganshev <spoganshev@gmail.com>
2025-09-25 02:18:09 +0300

    feat(obsidian.lua): added configuration to create notes with proper names and additional key bindings

--- lua/kickstart/plugins/obsidian.lua
+++ lua/kickstart/plugins/obsidian.lua
@@ -1,7 +1,18 @@
 -- https://github.com/obsidian-nvim/obsidian.nvim
+
+---@param spec { id: string, dir: obsidian.Path, title: string|? }
+---@return string|obsidian.Path The full path to the new note.
+local note_path_func = function(spec)
+  local path = spec.dir / tostring(spec.title)
+  return path:with_suffix '.md'
+end
+
 return {
   'obsidian-nvim/obsidian.nvim',
   lazy = false,
+  dependencies = {
+    'saghen/blink.cmp',
+  },
   version = '*', -- recommended, use latest release instead of latest commit
   -- ft = 'markdown',
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
@@ -24,8 +35,34 @@ return {
     },
     legacy_commands = false,
     disable_frontmatter = true,
+    note_path_func = note_path_func,
+    notes_subdir = '100 Zettelkasten',
+    new_notes_location = 'notes_subdir',
+    templates = {
+      folder = '950 Templates',
+    },
+    completion = (function()
+      return {
+        nvim_cmp = false,
+        blink = true,
+        min_chars = 2,
+        match_case = true,
+        create_new = false,
+      }
+    end)(),
+    footer = {
+      -- cool feature, but leads to "too many open files issue"
+      enabled = false,
+    },
   },
   keys = {
+    { '<leader>oo', ':Obsidian<CR>', desc = 'Obsidian command picker', silent = true },
     { '<leader>os', ':Obsidian quick_switch<CR>', desc = 'Obsidian Quick Switch', silent = true },
+    { '<leader>oT', ':Obsidian new_from_template<CR>', desc = 'Create Obsidian note from template', silent = true },
+    { '<leader>ot', ':Obsidian template<CR>', desc = 'Obsidian: insert template', silent = true },
+    { '<leader>oc', ':Obsidian toc<CR>', desc = 'Obsidian: Table of Contents', silent = true },
+    { '<leader>ol', ':Obsidian links<CR>', desc = 'Obsidian: Links', silent = true },
+    { '<leader>ob', ':Obsidian backlinks<CR>', desc = 'Obsidian: Backlinks', silent = true },
+    { '<leader>oO', ':Obsidian open<CR>', desc = 'Obsidian: Open in app', silent = true },
   },
 }
