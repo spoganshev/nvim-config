160968366f6fa0c595acae97bed47881edef2f8b
Sergei Poganshev <spoganshev@gmail.com>
2025-09-27 23:17:58 +0300

    feat(todo-comments.lua): configure keymaps

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,27 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+    keys = {
+      {
+        ']t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next todo comment',
+      },
+      {
+        '[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous todo comment',
+      },
+      { '<leader>st', '<cmd>TodoTelescope<cr>', desc = '[S]earch TODO comments' },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
