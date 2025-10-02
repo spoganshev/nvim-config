db4b81df052ae7ee857836f35300e706c3230d7f
Sergei Poganshev <spoganshev@gmail.com>
2025-10-03 01:51:24 +0300

    fix(autopairs.lua): use already existing autopairs.lua

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -101,10 +101,5 @@ return {
       signature = { enabled = true },
     },
   },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-  },
 }
 -- vim: ts=2 sts=2 sw=2 et
