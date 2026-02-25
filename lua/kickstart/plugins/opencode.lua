6d50158f00f57c2e7828e893e6d4dc9e7beb7c83
Sergei Poganshev <sergei.poganshev.consultant@nielseniq.com>
2026-02-25 17:19:42 +0200

    fix(opencode): update configuration according to new version guidelines

--- lua/kickstart/plugins/opencode.lua
+++ lua/kickstart/plugins/opencode.lua
@@ -2,22 +2,38 @@
 -- https://github.com/nickjvandyke/opencode.nvim
 
 return {
-  'NickvanDyke/opencode.nvim',
+  'nickjvandyke/opencode.nvim',
+  version = '*', -- Latest stable release
   dependencies = {
-    -- Recommended for `ask()` and `select()`.
-    -- Required for `snacks` provider.
-    ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-    { 'folke/snacks.nvim', opts = { input = {}, picker = {}, terminal = {} } },
+    {
+      -- `snacks.nvim` integration is recommended, but optional
+      ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+      'folke/snacks.nvim',
+      optional = true,
+      opts = {
+        input = {}, -- Enhances `ask()`
+        picker = {  -- Enhances `select()`
+          actions = {
+            opencode_send = function(...)
+              return require('opencode').snacks_picker_send(...)
+            end,
+          },
+          win = {
+            input = {
+              keys = {
+                ['<a-a>'] = { 'opencode_send', mode = { 'n', 'i' } },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
       -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
       -- auto_fallback_to_embedded = false,
-      provider = {
-        enabled = 'tmux',
-        tmux = {},
-      },
     }
 
     -- Required for `opts.events.reload`.
